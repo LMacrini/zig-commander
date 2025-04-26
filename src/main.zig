5473be29777526769229072290dd830d6f45791c
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2025-04-25 21:29:09 -0400

    update example

--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,50 @@
 //! This file is just an example
 
-fn parseBool(_: []const u8) !*bool {
-    var b = false;
-    return &b;
-}
+const operations = enum {
+    add,
+    sub,
+};
 
-const silly = blk: {
-    var new_cmd = lib.Command.init("silly", .{
-        .description = "is silly",
-    });
+const math_cmd = blk: {
+    var new_cmd = lib.Command.init("math", .{});
 
-    _ = new_cmd.addArgument(u8, .{});
+    _ = new_cmd
+        .addArgument(operations, .{
+            .description = "do an operation",
+        })
+        .addArgument(isize, .{})
+        .addArgument(isize, .{});
 
     break :blk new_cmd;
 };
 
-const cmd = blk: {
+const main_cmd = blk: {
     var new_cmd = lib.Command.init("main", .{});
 
-    _ = new_cmd.addOption(u8, "ball", 8, .{})
-        .addOption(bool, "enable", true, .{
-            .short = 'e',
-            .description = "enables the switch",
-            .parser = &parseBool,
-        })
-        .addCommand(silly);
+    const help_cmd = lib.Command.init("help", .{
+        .description = "prints the help",
+    });
+
+    _ = new_cmd
+        .addCommand(help_cmd)
+        .addCommand(math_cmd);
 
     break :blk new_cmd;
 };
+
+fn math(cmd: math_cmd.ParsedType()) !void {
+    const stdout = std.io.getStdOut().writer();
+
+    const a = cmd.args[1];
+    const b = cmd.args[2];
+
+    const res = switch (cmd.args[0]) {
+        .add => a + b,
+        .sub => a - b,
+    };
+
+    try stdout.print("{d}\n", .{res});
+}
 
 pub fn main() !void {
     var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
@@ -37,13 +54,16 @@ pub fn main() !void {
 
     const allocator = debug_allocator.allocator();
 
-    var res = cmd.parse(allocator) catch |err| {
+    var res = main_cmd.parse(allocator) catch |err| {
         std.log.err("{s}", .{@errorName(err)});
         std.process.exit(1);
     };
     defer res.deinit();
 
-    std.debug.print("{any}\n", .{res.subcommand.silly.args[0]});
+    switch (res.subcommand) {
+        .help => std.log.info("this is the help", .{}),
+        .math => |cmd| try math(cmd),
+    }
 }
 
 const std = @import("std");
